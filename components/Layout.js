e5a77ba29aee87d20d1d6dacaaa1d97c8f989b4f
Chris Padilla <cdpadilla42@gmail.com>
2020-12-01 10:23:35 -0600

    Initial commit. Set up nextjs and apollo server.

---
+++ components/Layout.js
@@ -0,0 +1,39 @@
+import Head from 'next/Head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <nav>
+            <Link href="/">
+              <a>Home</a>
+            </Link>{' '}
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </nav>
+          {children}
+        </main>
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
