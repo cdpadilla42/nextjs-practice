9f76f89a209966da3091ca5e8755bf5aad0c6832
Chris Padilla <cdpadilla42@gmail.com>
2020-12-01 12:34:37 -0600

    Add dates to tracker ui

--- components/IndividualHabit.js
+++ components/IndividualHabit.js
@@ -1,18 +1,33 @@
 import HabitButton from './HabitButton';
 
 const IndividualHabit = ({ habit }) => {
+  const dates = getLast5Days();
+  console.log(dates);
   return (
     <article>
       <h3>{habit}</h3>
-      <dib>
-        <HabitButton>0</HabitButton>
-        <HabitButton>0</HabitButton>
-        <HabitButton>X</HabitButton>
-        <HabitButton>0</HabitButton>
-        <HabitButton>0</HabitButton>
-      </dib>
+      <div>
+        {dates.map((day) => {
+          const [month, date] = day.toLocaleDateString('en-US').split('/');
+          return (
+            <>
+              <span>{month + '/' + date}</span>
+              <HabitButton>0</HabitButton>
+            </>
+          );
+        })}
+      </div>
     </article>
   );
+};
+
+const getLast5Days = () => {
+  const dates = '01234'.split('').map((day) => {
+    const tempDate = new Date();
+    tempDate.setDate(tempDate.getDate() - day);
+    return tempDate;
+  });
+  return dates;
 };
 
 export default IndividualHabit;
