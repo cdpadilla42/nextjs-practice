d3d286f0a66a32fc711c3e6ece4d0336f2322654
Chris Padilla <cdpadilla42@gmail.com>
2020-12-02 22:17:20 -0600

    Connect addHabit mutation to db

--- api/habits/mutations.js
+++ api/habits/mutations.js
@@ -1,7 +1,14 @@
+import Habit from './habits';
+
 export const habitsMutations = {
   Mutation: {
     async addHabit(_, { habit }) {
-      console.log('add habit');
+      console.log('add habit', habit);
+      const newHabit = new Habit(habit);
+      newHabit.save((err, newHabit) => {
+        if (err) return console.error(err);
+        console.log('saved', newHabit);
+      });
     },
   },
 };
